79ee175b256c52b5596f9bd0f28b043801e94b53
Augustyn <AugustynWilk@gmail.com>
2020-06-07 16:59:57 +0200

    Resolves #First rest endpoint

---
+++ src/main/java/com/example/quarkus/app/Routing.java
@@ -0,0 +1,23 @@
+package com.example.quarkus.app;
+
+
+import io.quarkus.vertx.web.Route;
+import io.quarkus.vertx.web.RoutingExchange;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.ext.web.RoutingContext;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Routing {
+
+    @Route(path="/hello", methods = HttpMethod.GET)
+    void hello(RoutingContext rc) {
+        rc.response().end("hello");
+    }
+
+    @Route(path = "/greetings", methods = HttpMethod.GET)
+    void greetings(RoutingExchange ex) {
+        ex.ok("hello " + ex.getParam("name").orElse("world"));
+    }
+}
